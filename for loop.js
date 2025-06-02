204bf8560e58cb4673de0a7159c73bcd2ae4b174
devloper-jp <jnlpansuriya@gmail.com>
2025-06-02 14:19:51 +0530

    dream car

--- for loop.js
+++ for loop.js
@@ -1,4 +1,10 @@
-for (let bmw = 0; bmw <= 10; bmw++) {
-    console.log("bmw=",bmw);
-    }
-console.log ("loop finished");
+let bmw = 0;
+let audi = 0;
+let mercedes = 0;  
+
+for (let i = 0; i < 10; i++) {
+    bmw =  bmw + 1;
+    audi = audi + 2;
+    mercedes = mercedes + 3;
+    console.log("bmw = " ,bmw, "audi = ", audi, "mercedes = ", mercedes);
+}
